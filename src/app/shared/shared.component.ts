92f1a358b61175cb81972feb32326157a102ed9a
Navish <navish45@gmail.com>
2017-10-09 12:58:54 +0300

    Fixed SignIn and SignOut buttons on header

--- src/app/shared/shared.component.ts
+++ src/app/shared/shared.component.ts
@@ -9,10 +9,19 @@
     })
     export class SharedComponent implements OnInit, OnDestroy {
     
+  bodyClasses = 'skin-blue sidebar-mini';
+  body: HTMLBodyElement = document.getElementsByTagName('body')[0];
+
       ngOnInit() { 
+        // add the the body classes
+        this.body.classList.add('skin-blue');
+        this.body.classList.add('sidebar-mini');
     
       }
       ngOnDestroy() {
+        //remove the the body classes
+        this.body.classList.remove('skin-blue');
+        this.body.classList.remove('sidebar-mini');
       }
     
     }
