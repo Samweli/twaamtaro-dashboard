427e38d1f5b11eaea4a5c32ffc0b1c4e9f04d016
Navish <navish45@gmail.com>
2017-10-25 10:27:23 +0300

    fixed removing loginbox after login and generate graph button

--- src/app/shared/admin/admin.component.ts
+++ src/app/shared/admin/admin.component.ts
@@ -11,10 +11,15 @@ export class AdminComponent implements OnInit, OnDestroy {
   bodyClasses = 'skin-blue sidebar-mini';
   body: HTMLBodyElement = document.getElementsByTagName('body')[0];
 
+  closelogin(){ 
+    document.getElementById('loginmodal').style.display='none';
+  }
+
   ngOnInit() { 
     // add the the body classes
     this.body.classList.add('skin-blue');
     this.body.classList.add('sidebar-mini');
+    this.closelogin();
 
   }
   ngOnDestroy() {
