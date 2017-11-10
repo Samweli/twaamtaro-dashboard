22a1e422dcb41466bf931d709bd8a86aafd08991
Navish <navish45@gmail.com>
2017-11-10 12:55:42 +0300

    moved translation buttons to sidebar

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -2,14 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
 
 import {AuthService} from "./../../core/auth.service";
-import {TranslateService} from "../../transilate/translate.service";
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
-  constructor(private router: Router,public authService: AuthService, private _translate: TranslateService) { }
+  constructor(private router: Router,public authService: AuthService) { }
 
   loginbox() {
     document.getElementById('loginmodal').style.display='block';
@@ -29,45 +28,9 @@ export class HeaderComponent implements OnInit {
       this.loggedIn = this.authService.isLoggedIn();
       return this.loggedIn;
   }
-  public translatedText: string;
-  public supportedLanguages: any[];
-  supportedLangs: any;
-
+  
   ngOnInit() {
-    // standing data
-    this.supportedLangs = [
-      {display: 'English', value: 'en'},
-      {display: 'Swahili', value: 'sw'},
-    ];
     this.isLoggedIn();
-    this.selectLang('sw');
-
-
-    this.subscribeToLangChanged();
-
-    // set language
-    this._translate.setDefaultLang('en');
-    this._translate.enableFallback(true);
-    this.selectLang('sw');
-
-  }
-
-  isCurrentLang(lang: string) {
-    return lang === this._translate.currentLang;
-  }
-
-  selectLang(lang: string) {
-    // set default;
-    this._translate.use(lang);
-    // this.refreshText(); // remove
-  }
-
-  refreshText() {
-    this.translatedText = this._translate.instant('all');
-  }
-
-  subscribeToLangChanged() {
-    return this._translate.onLangChanged.subscribe(x => this.refreshText());
   }
 
 
