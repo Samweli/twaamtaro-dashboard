7789a39b702e09d6e6596332db3fa2ffc61cfe8f
Mloweedgar <mloweedgar0@gmail.com>
2017-12-08 15:09:49 +0300

    filtering need helps drains based on region

--- src/app/shared/drains/help-drain/help-drain.component.ts
+++ src/app/shared/drains/help-drain/help-drain.component.ts
@@ -1,11 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit} from '@angular/core';
 
 import {AuthService} from "./../../../core/auth.service";
 import { DrainsService } from './../../../core/drains.service';
 import { PagerService } from './../../../core/paging.service';
-
-import * as _ from 'underscore';
-import { Drain } from './../drain';
 
 import { NgProgress } from 'ngx-progressbar';
 
@@ -20,7 +17,7 @@ export class HelpDrainComponent implements OnInit {
   created: any;
   daysGone: any;
   dateCreated: any;
-  drains: any;
+  drains: any = [];
   errMsg: any; // Error Message
   helpCategory : any;
   helpNeeded : any;
@@ -30,16 +27,17 @@ export class HelpDrainComponent implements OnInit {
   pagedDrains: any[]; // paged drains
   thedrain: any;
   today: any;
+  searchKey: any;
 
   constructor(
-    private drainService: DrainsService, 
-    public authService: AuthService, 
+    private drainService: DrainsService,
+    public authService: AuthService,
     private pagerService: PagerService,
     public ngProgress: NgProgress,
   ) { }
 
   getDuration(d)
-  { 
+  {
     var minutes = 1000 * 60;
     var hours = minutes * 60;
     var days = hours * 24;
@@ -54,7 +52,7 @@ export class HelpDrainComponent implements OnInit {
   }
 //Fetches all drains in need of help and their details
   getFilteredDrains(status?): any {
-    this.ngProgress.start();    
+    this.ngProgress.start();
     this.drainService
       .getFilteredHelp(status)
       .subscribe(
@@ -63,6 +61,16 @@ export class HelpDrainComponent implements OnInit {
     this.setPage(1);
     this.ngProgress.done();
     });
+  }
+
+  drainFilter(data:any,key?):any{
+
+    console.log("inside parent component");
+
+    console.log(key);
+
+     this.pagedDrains = this.drains.filter(drain => drain.user.street[key.level] == key.event);
+    console.log(this.drains);
   }
 
   setPage(page: number) {
@@ -75,13 +83,13 @@ export class HelpDrainComponent implements OnInit {
 
     // Get current page of items
     this.pagedDrains = this.drains.slice(this.pager.startIndex, this.pager.endIndex + 1);
-  } 
- 
+  }
+
   //Get extra details of the requested help
 
 
   helpmodal(gid,category,help)
-  { 
+  {
     this.thedrain = gid;
     this.helpCategory = category;
     this.helpNeeded = help;
@@ -90,7 +98,7 @@ export class HelpDrainComponent implements OnInit {
 
   //Close the helpmodal by clicking anywhere else in the page
   closedetails()
-  { 
+  {
     var modal = document.getElementById('helpdetails');
     window.onclick = function(event) {
         if (event.target == modal) {
@@ -101,19 +109,19 @@ export class HelpDrainComponent implements OnInit {
 
   //Close helpmodal by clicking the close button
   closemodal()
-  { 
+  {
     document.getElementById('helpdetails').style.display='none';
   }
   showFilters()
-  { 
+  {
     var x = document.getElementById("mobileFilters");
     if (x.className.indexOf("w3-show") == -1) {
         x.className += " w3-show";
-    } else { 
+    } else {
         x.className = x.className.replace(" w3-show", "");
     }
   }
-  
+
   isLoggedIn()
   {
       this.loggedIn = this.authService.isLoggedIn();
--- src/app/shared/regional-filters/regional-filters.component.ts
+++ src/app/shared/regional-filters/regional-filters.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {StreetService} from "../../core/streets.service";
 
 @Component({
   selector: 'app-regional-filters',
@@ -7,9 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegionalFiltersComponent implements OnInit {
 
-  constructor() { }
+  @Output()
+    selectValueChaged: EventEmitter<any> = new EventEmitter();
+  streets: any = [];
+  selectedStreet: any ={};
+
+  constructor(private streetService: StreetService) { }
+
+  onChange($event,level?){
+    console.log("inside child component");
+    console.log($event);
+    this.selectValueChaged.emit({event: $event, level: level});
+
+  }
+  getStreets(){
+    this.streetService.getStreets()
+      .subscribe(res => {
+        this.streets = res;
+        console.log('streets');
+        console.log(res);
+      })
+  }
 
   ngOnInit() {
+    this.getStreets()
   }
 
 }
