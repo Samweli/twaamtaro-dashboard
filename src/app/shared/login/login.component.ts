998fc00710909f256fe79591403aaa8d32519a3e
Navish <navish45@gmail.com>
2017-12-13 12:00:20 +0300

    fixed login after API changes

--- src/app/shared/login/login.component.ts
+++ src/app/shared/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from "@angular/router";
+import { NgProgress } from 'ngx-progressbar';
 import { AuthService } from "./../../core/auth.service";
 import { FormErrorsService } from "./../../core/form-errors.service";
 import { SessionService } from "../../core/session.service";
@@ -13,6 +14,7 @@ export class LoginComponent implements OnInit {
   constructor (
         public authService: AuthService,
         public formErrorsService: FormErrorsService,
+        public ngProgress: NgProgress,
         public router: Router,
         public sessionService: SessionService ) { }
   
@@ -20,17 +22,24 @@ export class LoginComponent implements OnInit {
   loginStatus: any;
   theUser: any;
   user: any = { 'sms_number': '','password': '' };
-  
-  login() {
-    this.authService.login(this.user)
-    .subscribe(res => {
-      this.userData = this.authService.userdata;
-      },
-      error => {
-         this.formErrorsService.error(error);
-      });
+  loginCalled: any = false;
 
-    }
+  login() {
+    this.ngProgress.start();
+    
+    this.authService.login(this.user)
+      .subscribe(res => {
+
+        this.loginCalled = true;
+        this.userData = this.authService.userdata;
+
+        
+      }, error => {
+        this.loginCalled = true;
+        this.formErrorsService.error(error);
+      });
+    this.ngProgress.done();
+  };
 
   logout() { 
    this.authService.logout();
