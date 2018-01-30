0283320ff5b632b942748dfe2933d8f47999546b
Navish <navish45@gmail.com>
2018-01-30 12:31:30 +0300

    moved the language links to the header

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
+import { TranslateService } from "../../translate/translate.service";
 
 import { AuthService } from "./../../core/auth.service";
 import { UserService } from "./../../core/user.service";
@@ -14,9 +15,14 @@ export class HeaderComponent implements OnInit {
     private router: Router,
     public authService: AuthService,
     public userService: UserService,
-    private sessionService: SessionService
+    private sessionService: SessionService,
+    private _translate: TranslateService
 
   ) { }
+
+  public translatedText: string;
+  public supportedLanguages: any[];
+  supportedLangs: any;
 
   loginbox() {
     document.getElementById('loginmodal').style.display='block';
@@ -54,11 +60,45 @@ export class HeaderComponent implements OnInit {
       this.leaderReq({role_name:'weo'});
     }
   }
+ 
 
 
   ngOnInit() {
     this.isLoggedIn();
     this.checkNotification();
+
+   // standing data
+   this.supportedLangs = [
+    {display: 'English', value: 'en'},
+    {display: 'Swahili', value: 'sw'},
+  ];
+
+  this.selectLang('sw');
+
+  this.subscribeToLangChanged();
+
+  // set language
+  this._translate.setDefaultLang('en');
+  this._translate.enableFallback(true);
+  this.selectLang('sw');
+  }
+
+  isCurrentLang(lang: string) {
+    return lang === this._translate.currentLang;
+  }
+
+  selectLang(lang: string) {
+    // set default;
+    this._translate.use(lang);
+    // this.refreshText(); // remove
+  }
+
+  refreshText() {
+    this.translatedText = this._translate.instant('all');
+  }
+
+  subscribeToLangChanged() {
+    return this._translate.onLangChanged.subscribe(x => this.refreshText());
   }
 
 
