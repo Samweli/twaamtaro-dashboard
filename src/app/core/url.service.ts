0c7fcc7d26a02b982caaf388a4da4af62a7ba166
Mloweedgar <mloweedgar0@gmail.com>
2018-02-05 15:58:59 +0300

    setting base url to production url

--- src/app/core/url.service.ts
+++ src/app/core/url.service.ts
@@ -5,7 +5,7 @@ export class UrlService {
   // base url variables
   prodUrl: string = 'http://twaamtaro.org/api/v1/';
   devUrl: string = 'http://localhost:3000/api/v1/';
-  baseUrl:string = this.devUrl;
+  baseUrl:string = this.prodUrl;
 
   // auth related url
   public loginUrl = this.baseUrl + 'sessions';
